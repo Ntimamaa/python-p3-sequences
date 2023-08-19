7ee475c470d7210e66354eb396681328a6e658ea
Drake Tempes <dreake.ntimama@student.moringaschool.com>
2023-08-19 09:44:26 +0300

    Completed assignment

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,15 @@
 #!/usr/bin/env python3
-
 def print_fibonacci(length):
-    pass
+    fibonacci_sequence = []
+    a, b = 0, 1
+    
+    for _ in range(length):
+        fibonacci_sequence.append(a)
+        a, b = b, a + b
+    
+    print(fibonacci_sequence)
+
+print_fibonacci(0)  # Output: []
+print_fibonacci(1)  # Output: [0]
+print_fibonacci(9)  # Output: [0, 1, 1, 2, 3, 5, 8, 13, 21]
+
